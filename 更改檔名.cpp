89ce618edea9880a3174e58ac2c70eac1cdadca6
s1130343-coder <s1130343@dmhs.kh.edu.tw>
2026-03-31 11:54:33 +0800

    更改檔名

---
+++ 更改檔名.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string s = "happy,newyear,enjoy";
+   for (int i=0; i < s.size(); i++) {
+    	if(s [i] == ','){
+    	   s [i]  = ' ';
+    	}
+    	cout << s << endl;
+    }
+	return 0;
+}
